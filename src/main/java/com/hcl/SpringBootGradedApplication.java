535a32c079ec26a1543b0503c6cba48719ffd175
Jammy378 <85183360+Jammy378@users.noreply.github.com>
2021-06-07 16:14:13 +0530

    Update SpringBootGradedApplication.java

--- src/main/java/com/hcl/SpringBootGradedApplication.java
+++ src/main/java/com/hcl/SpringBootGradedApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootGradedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGradedApplication.class, args);
+		//SpringApplication.run(SpringBootGradedApplication.class, args);
 		System.out.println("Hello.. DevOps!");
 	}
 
